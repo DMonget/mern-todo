d151813cf8d63daa921da6204518cbd8af3c7a46
DMonget <you@example.com>
2020-04-15 19:14:12 +0200

    Prepare the application's routes.

---
+++ routes/api.js
@@ -0,0 +1,8 @@
+const express = require ('express');
+const router = express.Router();
+
+router.get('/todos', (res, res, next) => {});
+router.post('/todos', (res, res, next) => {});
+router.delete('/todos/:id', (res, res, next) => {});
+
+module.exports = router;
